d1cab278d1d60e6b6ef53be3e64aed87f0f9d468
titulus <titulus.web@gmail.com>
2016-08-31 12:37:05 +0300

    api request successfull

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@
 
 const settings = require('./settings.json');
 
+const request = require('request');
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(settings.bot_token, {polling: true});
 
@@ -10,3 +11,14 @@ bot.on('message', function (msg) {
     var fromId = msg.chat.id;
     bot.sendMessage(fromId, 'asd');
 });
+
+request.post(
+    settings.api_url,
+    { json: { token: settings.api_token } },
+    function (error, response, body) {
+        console.log(body);
+        // if (!error && response.statusCode == 200) {
+        //     console.log(body)
+        // }
+    }
+);
