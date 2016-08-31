0b9a15305a96f7ceafb03b79a9723a4091aea2c5
titulus <titulus.web@gmail.com>
2016-08-31 12:27:05 +0300

    first message from bot

--- bot.js
+++ bot.js
@@ -4,3 +4,9 @@ const settings = require('./settings.json');
 
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(settings.bot_token, {polling: true});
+
+bot.on('message', function (msg) {
+    console.log(msg);
+    var fromId = msg.chat.id;
+    bot.sendMessage(fromId, 'asd');
+});
