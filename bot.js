4cf539e68fb08cedde508e9c6570db568f8e3141
titulus <titulus.web@gmail.com>
2016-08-31 12:16:12 +0300

    add settings and run bot

--- bot.js
+++ bot.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const settings = require('./settings.json');
+
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(settings.bot_token, {polling: true});
